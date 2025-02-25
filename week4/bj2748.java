319474eba6e656e735adacb02e66dc8315590d26
Gksrufdk09 <rufdk09@naver.com>
2025-02-25 23:38:31 +0900

    week4 day\2

---
+++ week4/bj2748.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class bj2748 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n + 1]; // 피보나치 수 저장 배열
+        dp[1] = 1;
+        if (n > 1) dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
